a3ee84066b066e76c2c480cb76e3a339dd828497
edoardo de parolis <edodeparolis@gmail.com>
2022-09-20 00:06:02 +0200

    struttura progetto

---
+++ src/main/java/it/polimi/progettoIngSoft/TrackingPlatform/TrackingPlatformApplication.java
@@ -0,0 +1,13 @@
+package it.polimi.progettoIngSoft.TrackingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/polimi/progettoIngSoft/TrackingPlatform/TrackingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.progettoIngSoft.TrackingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
